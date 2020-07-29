145e65983482d4d311c4bddfb428b0f6d124a83f
An Phuc Thien Nguyen <npta97@gmail.com>
2020-07-29 17:03:29 +0700

    Init project

---
+++ src/test/java/com/elsevier/elsevierzuulgateway/ElsevierZuulGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsevier.elsevierzuulgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsevierZuulGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
